b0458ee11ca7c113e9ae6d83d9bd0b643002cd1d
takiemojeżycie <98774757+takiemojezycie@users.noreply.github.com>
2022-04-07 10:49:46 +0200

    Create lesson3.cpp

---
+++ lesson3.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+    /*
+      double x;
+    cout << "Podaj liczbę kilomterów: ";
+    cin >> x;
+    if (x < 0)
+        {
+            cout << "Kilometry nie mogą być ujemne!";
+        }
+    else
+        {
+            cout << "Dobrze!"<< endl;
+            cout << x << "km to " << x * 1000 << "m";
+        }
+    */
+  
+  
+  int x;
+  cout << "Podaj liczbę: ";
+  cin >> x;
+  // operatory logiczne == != >= <= > <
+  if (x >= 0)
+    {
+      cout << "|x| = " << x << endl;
+    }
+  else
+    {
+      x = x * (-1);
+      cout << "|x| = " << endl;
+    }
+  int rok;
+  int lata;
+
+
+  cout << "Podaj rok urodzenia: ";
+  cin >> rok;
+  if (rok > 2022)
+    {
+      cout << "To niemozliwe!" << endl;
+      cout << "Podaj rok urodzenia jeszcze raz: ";
+      cin >> rok;
+      lata = 2022 - rok;
+      if (lata >= 18)
+    	{
+    	  cout << "Jestes pelnoletni, masz " << lata << " lat";
+    	}
+          else
+    	{
+    	  cout << "Jestes niepelnoletni, masz " << lata << " lat";
+    	}
+    }
+    else
+    {
+        lata = 2022 - rok;
+
+        if (lata >= 18)
+            {
+                cout << "Jestes pelnoletni, masz " << lata << " lat";
+            }
+        else
+            {
+              cout << "Jestes niepelnoletni, masz " << lata << " lat";
+            }
+    }
+
+
+
+
+  return 0;
+  
+  return 0;
+}
