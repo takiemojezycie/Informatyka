dfc68729dde5cbf83fd838878fa63d38937383c6
żeglarz partiota <98774757+zeglarzpartiota@users.noreply.github.com>
2023-02-13 11:39:26 +0100

    Create przedzialy.cpp

---
+++ przedzialy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+double f(double x) {
+    return x*(x*(x-3)+2)-6;
+}
+
+int main() {
+    
+    double a=0, b=20;
+    
+    if not (((a+b) / 2) == 0) {
+        if ((f(a) * f((a + b) / 2)) < 0)
+    }
+     
+
+    return 0;
+}
