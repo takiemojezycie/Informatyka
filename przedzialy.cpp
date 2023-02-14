4902d422d5b44edc9fd72248fccb4f15a1c5b49d
żeglarz partiota <98774757+zeglarzpartiota@users.noreply.github.com>
2023-02-14 22:10:45 +0100

    Update przedzialy.cpp

--- przedzialy.cpp
+++ przedzialy.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 double f(double x) {
-    return x*(x*(x-3)+2)-6;
+    return 0.2 * x - 2.4;
 }
 
-int main() {
-    
-    double a=0, b=20;
-    
-    if not (((a+b) / 2) == 0) {
-        if ((f(a) * f((a + b) / 2)) < 0)
+double dzielenie(double a, double b, double dokladnosc) {
+
+    if (f(a) == 0) {
+            return a;
+        }
+
+        if (f(b) == 0) {
+            return b;
+        }
+
+    double srodek;
+
+    while(b - a > dokladnosc) {
+
+        srodek = (a + b) / 2;
+
+        if (f(srodek) == 0) {
+            return srodek;
+        }
+
+        if (f(a) * f(srodek) < 0) {
+            b = srodek;
+        }
+        else {
+            a = srodek;
+        }
+
     }
-     
+
+    return (a+b)/2;
+}
+
+int main(){
+
+    cout << "Miejsce zerowe to: " << setprecision(5) << dzielenie(2, 100, 0.00001);
 
     return 0;
 }
